2b8dbd0d3432165489f242f1643edf774823823b
Victor Rivero <frovic.ve@gmail.com>
2026-02-06 16:28:05 -0400

    avances

---
+++ database/migrations/2025_08_26_043904_create_departament_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departaments', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->string('address')->nullable();
+            $table->string('block')->nullable();
+            $table->float('area');
+            $table->string('description');
+            $table->string('code_pay')->nullable();
+            $table->string('floor');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->nulleable()->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departament');
+    }
+};
